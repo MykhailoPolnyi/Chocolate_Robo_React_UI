76278b9a085aab724ea0b71966764365b9f44069
Meksich <maks10031003@gmail.com>
2023-10-28 15:35:55 +0300

    Add arrows to handle inputs

--- src/mainWindow/MainWindow.js
+++ src/mainWindow/MainWindow.js
@@ -1,23 +1,48 @@
-import logo from '../res/logo.svg';
 import './MainWindow.css';
+import { ArrowButton } from './MainWindow.styles.js';
+import arrowUp from '../res/arrowUp.png';
+import arrowDown from '../res/arrowDown.png';
+import arrowLeft from '../res/arrowLeft.png';
+import arrowRight from '../res/arrowRight.png';
+
 
 function MainWindow() {
   return (
     <div className="MainWindow">
-      <header className="MainWindow-header">
-        <img src={logo} className="MainWindow-logo" alt="logo" />
-        <p>
-          Edit <code>src/MainWindow.js</code> and save to reload.
-        </p>
-        <a
-          className="MainWindow-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tr>
+            <th></th>
+            <th>
+              <ArrowButton>
+                <img src={arrowUp}></img>
+              </ArrowButton>
+            </th>
+            <th></th>
+        </tr>
+        <tr>
+            <td>
+              <ArrowButton>
+                <img src={arrowLeft}></img>
+              </ArrowButton>
+            </td>
+            <td></td>
+            <td>
+              <ArrowButton>
+                <img src={arrowRight}></img>
+              </ArrowButton>
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>
+              <ArrowButton>
+                <img src={arrowDown}></img>
+              </ArrowButton>
+            </td>
+            <td></td>
+        </tr>
+      </table>
+      
     </div>
   );
 }
---
+++ src/mainWindow/MainWindow.styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const ArrowButton = styled.button`
+background-color: white;
+    padding: 0;
+    width: 105px;
+    height: 105px;
+    :hover{
+        background-color: #D9d9dc;
+    }
+`
