f41e1d1ada4876d8ce23afc54310aeb62ff6b7c8
Meksich <maks10031003@gmail.com>
2023-10-28 13:12:46 +0300

    Reorganizing folders

--- src/mainWindow/MainWindow.test.js
+++ src/mainWindow/MainWindow.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import MainWindow from './MainWindow';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<MainWindow />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
