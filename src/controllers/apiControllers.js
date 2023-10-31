6727a0e2efe537d60da65b0e079df5007db26bef
Meksich <maks10031003@gmail.com>
2023-10-31 15:49:10 +0200

    axios => fetch

--- src/controllers/apiControllers.js
+++ src/controllers/apiControllers.js
@@ -14,12 +14,12 @@ const http = axios.create({
 export const getRotationStart = async (baseUrl, direction) => {
     
     console.log(`${baseUrl}/command?action=${direction}`)
-    const rawResponse = await http.get(`${baseUrl}/command?action=${direction}`);
+    const rawResponse = await fetch(`${baseUrl}/command?action=${direction}`);
     return rawResponse.data;
 }
 
 export const getRotationStop = async (baseUrl) => {
     console.log(`${baseUrl}/command?action=stop`)
-    const rawResponse = await http.get(`${baseUrl}/command?action=stop`);
+    const rawResponse = await fetch(`${baseUrl}/command?action=stop`);
     return rawResponse.data;
 }
